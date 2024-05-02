98b78462a441b3e7f1933158f9994966f421e3f1
greales <g.realesguti@gmail.com>
2024-05-02 17:27:20 +0200

    add: sh files

--- scomsoltest.sh
+++ scomsoltest.sh
@@ -2,6 +2,6 @@
 echo "Hello, World!"
 
 module load comsol matlab
-comsol server &
-matlab -batch "addpath('/apps/generic/comsol/6.2/mli/');pause(10);disp('OK1');mphstart(2036);disp('OK2');import com.comsol.model.util.*;modelname='MPH/Scintillator3D_1DStudy_2Dgeomv2 - Copy';mphopen(modelname);exit;" 
+comsol server -np 2 &
+matlab -batch "DesignSpaceStudy1D('test','Wm+We')" 
 
