ba39f1bfa8cad1bba80a8caf1f02203eb5561ad7
greales <g.realesguti@gmail.com>
2024-06-12 19:36:29 +0200

    fix: OptND input etc

--- OptimizationND.m
+++ OptimizationND.m
@@ -1,4 +1,4 @@
-function OptimizationND(savename,objective_name,server, Volcon, optimizer, x0_file, varargin)
+function OptimizationND(savename,objective_name,server, optimizer, varargin)
     % VolCon Default = 1.707000e-04
 
     % Create a global structure to store iteration data
@@ -8,35 +8,92 @@ function OptimizationND(savename,objective_name,server, Volcon, optimizer, x0_fi
     iterData.fval = [];
     iterData.x = [];
 
-    fprintf('### 1D OPTIMIZATION MATLAB ###\n')
-    % Default values for minmesh and maxmesh
-    defaultMinMesh = 0.00075;
-    defaultMaxMesh = 0.0015;
-    defaultI0=0.005;
-    defaultIend=0.9;
-    
-    if nargin < 7
-        fprintf("Number of inputs, %i\n",nargin)
-        minmesh = defaultMinMesh;
-        fprintf("MinMesh, %f\n",minmesh)
-        maxmesh = defaultMaxMesh;
-        fprintf("MaxMesh, %f\n",maxmesh)
-        SimpStol=1e-6;
-        I0=defaultI0;
-        Iend=defaultIend;
-    else
-        fprintf("Number of inputs, %i\n",nargin)
-        minmesh = varargin{1};
-        fprintf("MinMesh, %f\n",minmesh)
-        maxmesh = varargin{2};
-        fprintf("MaxMesh, %f\n",maxmesh)
-        SimpStol=varargin{3};
-        I0=varargin{4};
-        Iend=varargin{5};
 
-    end
-    fprintf(append('Objective name: ',objective_name,'\n'))
-    fprintf(append('Optimizer name: ',optimizer,'\n'))
+
+    %DesignSpaceStudy1D('savefile', 'We', 0, 100, 'minmesh', 0.00075, 'maxmesh', 0.0015, 'SimpStol', 1e-5, 'I0', 0.005, 'Iend', 0.9, 'Ampl', 1E7, 'x0',0.75,'xend',1.25);
+
+    fprintf('### 1D OPTIMIZATION MATLAB ###\n');
+    
+    %Default values for optional parameters
+    defaultMinMesh = 0.00075;
+    defaultVolCon = 1.707000e-04;
+    defaultMaxMesh = 0.0015;
+    defaultSimpStol = 1e-6;
+    defaultI0 = 0.005;
+    defaultIend = 0.9;
+    defaultAmpl = 1e7;
+    defaultx0 = 0.75;
+    defaultxend = 1.25;
+    defaulttr = 0.75;
+    defaulttd = 1.25;
+    defaultSurf = [11, 40];
+    defaultSurfin =  [23, 27];
+    defaultjRINDEX = 8.3559e-06;
+    defaultjRINDEX_G = 2.5710e-06;
+    defaultjRINDEX_R = 4.1779e-07;
+    defaultx0_file = '';
+
+    defaultmodelname = 'Scintillator3D_1DStudy_2Dgeomv2 - Copyv2.mph';
+    %Create input parser
+    p = inputParser;
+    
+    %Add required parameters
+    addRequired(p, 'savename', @ischar);
+    addRequired(p, 'objective_name', @ischar);
+    addRequired(p, 'server', @isnumeric);
+    addRequired(p, 'optimizer', @ischar);
+
+    %Add optional parameters with default values
+    addParameter(p, 'minmesh', defaultMinMesh, @isnumeric);
+    addParameter(p, 'maxmesh', defaultMaxMesh, @isnumeric);
+    addParameter(p, 'SimpStol', defaultSimpStol, @isnumeric);
+    addParameter(p, 'I0', defaultI0, @isnumeric);
+    addParameter(p, 'Iend', defaultIend, @isnumeric);
+    addParameter(p, 'Ampl', defaultAmpl, @isnumeric);
+    addParameter(p, 'x0', defaultx0, @isnumeric);
+    addParameter(p, 'xend', defaultxend, @isnumeric);
+    addParameter(p, 'tr', defaulttr, @isnumeric);
+    addParameter(p, 'td', defaulttd, @isnumeric);
+    addParameter(p, 'Surf', defaultSurf, @isnumeric);
+    addParameter(p, 'Surfin', defaultSurfin, @isnumeric);
+    addParameter(p, 'modelname', defaultmodelname, @ischar);
+    addParameter(p, 'jRINDEX', defaultjRINDEX, @isnumeric);
+    addParameter(p, 'jRINDEX_G', defaultjRINDEX_G, @isnumeric);
+    addParameter(p, 'jRINDEX_R', defaultjRINDEX_R, @isnumeric);
+    addParameter(p, 'x0_file', defaultx0_file, @isnumeric);
+    addParameter(p, 'volcon', defaultVolCon, @isnumeric);
+
+    %Parse inputs
+    parse(p, savename, objective_name, server,optimizer, varargin{:});
+    
+    %Retrieve values
+    minmesh = p.Results.minmesh;
+    maxmesh = p.Results.maxmesh;
+    SimpStol = p.Results.SimpStol;
+    I0 = p.Results.I0;
+    Iend = p.Results.Iend;
+    Ampl = p.Results.Ampl;
+    x0 = p.Results.x0;
+    xend = p.Results.xend;    
+    tr = p.Results.tr;
+    td = p.Results.td;   
+    Surf = p.Results.Surf;
+    Surf_in = p.Results.Surfin;   
+    modelname = p.Results.modelname;   
+    jRINDEX = p.Results.jRINDEX;
+    jRINDEX_G = p.Results.jRINDEX_G;
+    jRINDEX_R = p.Results.jRINDEX_R;
+    Volcon = p.Results.volcon;
+    x0_file = p.Results.x0_file;
+
+    %Display input values
+    fprintf('Number of inputs, %i\n', nargin);
+    fprintf('MinMesh, %f\n', minmesh);
+    fprintf('MaxMesh, %f\n', maxmesh);
+    fprintf('SimpStol, %f\n', SimpStol);
+    fprintf('I0, %f\n', I0);
+    fprintf('Iend, %f\n', Iend);
+   
 
     LibInitialization()
 	if server
@@ -50,19 +107,24 @@ function OptimizationND(savename,objective_name,server, Volcon, optimizer, x0_fi
     modelname = 'Scintillator3D_1DStudy_2Dgeom - Spline3.mph';
     
     % Define model constants
-    Surf = [7, 36];
-    Surf_in = [23, 19];
     %savename = 'Obj_WeWm_';
-    maxmeshsize_nominal = 0.0015;
-    minmeshsize_nominal = 0.00075;
+    maxmeshsize_nominal = maxmesh;
+    minmeshsize_nominal = minmesh;
     %objective_name = 'Wm+We';
   % objectiveFunctionSearch = Objective1DAdapSearch(minmeshsize_nominal, maxmeshsize_nominal, Surf, Surf_in, modelname, pltoption, objective_name, savename, SimpStol,I0,Iend);
     objectiveFunctionSearch = Objective1DAdapSearch(minmeshsize_nominal, maxmeshsize_nominal, Surf, Surf_in, modelname, pltoption, objective_name, savename, SimpStol,I0,Iend);
-
+    % Try setting the new parameters and catch errors
+    try
+        objectiveFunctionSearch.model.param.set('jRINDEX', jRINDEX);
+        objectiveFunctionSearch.model.param.set('jRINDEX_G', jRINDEX_G);
+        objectiveFunctionSearch.model.param.set('jRINDEX_R', jRINDEX_R);
+    catch ME
+        fprintf('Error setting jRINDEX parameters: %s\n', ME.message);
+    end
     xlim = [0.1,1.9];
     if isempty(x0_file)
         Nvar = 18;
-        x0 = ones(Nvar,1)*1; % Default value if x0 file is not provided
+        x0 = ones(Nvar,1)*0.95; % Default value if x0 file is not provided
     else
         % Read x0 from the provided file
         x0 = load(x0_file);
